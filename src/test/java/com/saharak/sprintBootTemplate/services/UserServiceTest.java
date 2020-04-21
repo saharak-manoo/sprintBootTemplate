25f06a18d4df1d2843527fcda88be4e84ae44c82
Saharak Manoo <Saharakmanoo@outlook.com>
2020-04-21 18:38:38 +0700

    [Updated] test

--- src/test/java/com/saharak/sprintBootTemplate/services/UserServiceTest.java
+++ src/test/java/com/saharak/sprintBootTemplate/services/UserServiceTest.java
@@ -4,7 +4,6 @@ import com.saharak.sprintBootTemplate.models.User;
 import com.saharak.sprintBootTemplate.repositories.UserRepository;
 
 import org.junit.Before;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
@@ -12,24 +11,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.Mock.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalToObject;
-import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.hamcrest.CoreMatchers.*;
 import static java.util.Arrays.asList;
-import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
